c38be2165e33075e2daa9a6951dec8027dba3b67
kilowiski <97659553+kilowiski@users.noreply.github.com>
2022-02-16 17:05:35 +0800

    add api url

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://d1597b41.au-syd.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
